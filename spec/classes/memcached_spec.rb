ed127713a2c31fb2a13b81c625d81f4a0c7895b1
fdammeke <fabian.dammekens@cegeka.be>
2012-11-12 10:57:28 +0100

    Initial memcached module

--- spec/classes/memcached_spec.rb
+++ spec/classes/memcached_spec.rb
@@ -3,5 +3,7 @@
 require 'spec_helper'
 
 describe 'memcached' do
-  it { should contain_class 'memcached' }
+  it { should contain_class('memcached') }
+  it { should contain_service('memcached').with_ensure('running') }
+  it { should contain_package('memcached').with_ensure('installed') }
 end
