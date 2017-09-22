158b1eafa4781348828d4a2538ae92b1af5bd45a
Fabian Dammekens <fabian.dammekens@cegeka.be>
2017-09-22 15:18:15 +0200

    Enable systemd

--- manifests/redhat/instance.pp
+++ manifests/redhat/instance.pp
@@ -45,13 +45,29 @@ define memcached::redhat::instance (
     notify  => Service["memcached${instance_name}"],
   }
 
-  file { "${memcached::params::init}${instance_name}":
-    ensure  => present,
-    content => template('memcached/init.erb'),
-    mode    => '0755',
-    require => [
-      Package['memcached'],
-    ],
-    notify  => Service["memcached${instance_name}"],
+  case $::operatingsystemmajrelease {
+    7: {
+      file { "/usr/lib/systemd/system/memcached${instance_name}.service":
+        ensure  => present,
+        content => template('memcached/systemd.erb'),
+      }
+      exec { "reload systemctl /usr/lib/systemd/system/memcached${instance_name}.service":
+        command   => '/bin/systemctl daemon-reload',
+        onlyif    => "/usr/bin/systemctl status memcached${instance_name} 2>&1 | /usr/bin/grep \"[c]hanged on disk\"",
+        subscribe => File["/usr/lib/systemd/system/memcached${instance_name}.service"],
+        notify    => Service["memcached${instance_name}"]
+      }
+    }
+    5,6: {
+    	file { "${memcached::params::init}${instance_name}":
+    	  ensure  => present,
+    	  content => template('memcached/init.erb'),
+    	  mode    => '0755',
+    	  require => [
+    	    Package['memcached'],
+    	  ],
+    	  notify  => Service["memcached${instance_name}"],
+    	}
+    }
   }
 }
